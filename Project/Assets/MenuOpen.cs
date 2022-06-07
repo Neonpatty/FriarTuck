18bfed83bf53457d3e99569319aab1bc3a35d6f4
Patrick Astermann <patrick.astermann01@gmail.com>
2022-06-07 14:31:34 +1000

    Updated Menu

---
+++ Project/Assets/MenuOpen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOpen : MonoBehaviour
+{
+    [SerializeField] private Animator menuAni;
+    private bool isOpen = false;
+
+    private void Awake()
+    {
+        menuAni = gameObject.GetComponent<Animator>();    
+    }
+
+    public void OpenMenu()
+    {
+        if (!isOpen)
+            menuAni.SetBool("clicked", true);
+        else
+            menuAni.SetBool("clicked", false);
+    }
+}
